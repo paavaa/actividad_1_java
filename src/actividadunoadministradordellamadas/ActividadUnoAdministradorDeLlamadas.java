ddb354d4b1ba0a7d8f0a08fc074122b8093308e9
Pablo Bedoya <bootcampablo@gmail.com>
2024-09-29 19:40:45 -0500

    create project and development first fase

---
+++ src/actividadunoadministradordellamadas/ActividadUnoAdministradorDeLlamadas.java
@@ -0,0 +1,16 @@
+
+package actividadunoadministradordellamadas;
+
+
+public class ActividadUnoAdministradorDeLlamadas
+{
+
+
+    public static void main(String[] args)
+    {
+        ManagmentCall tabla=new ManagmentCall();
+        tabla.setVisible(true);
+        tabla.setLocationRelativeTo(null);
+    }
+    
+}
